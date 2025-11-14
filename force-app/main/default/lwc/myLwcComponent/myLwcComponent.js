8b1053d261a44bb3af58147df4d23527c968e32a
danish-memon <129723635+danish-memon@users.noreply.github.com>
2025-11-14 16:39:44 +0530

    US-121 Added lwc comments

--- force-app/main/default/lwc/myLwcComponent/myLwcComponent.js
+++ force-app/main/default/lwc/myLwcComponent/myLwcComponent.js
@@ -1,3 +1,9 @@
+// Import the LightningElement base class from the LWC framework.
+// This provides core functionality for creating Lightning Web Components.
 import { LightningElement } from 'lwc';
 
-export default class MyLwcComponent extends LightningElement {}
+// Export the component class so it can be used in the Lightning app.
+// The class extends LightningElement, making it an LWC component.
+export default class MyLwcComponent extends LightningElement {
+    // Component logic will go here (properties, methods, getters, etc.)
+}
